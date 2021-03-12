b46d6ef5b44483d88f3dbeb2c5ef4f4404b40e79
Fede <chiellofederico@gmail.com>
2021-03-12 17:56:01 +0100

    dichiaro il costruttore

--- esercizio.php
+++ esercizio.php
@@ -5,4 +5,17 @@ class Movie {
     public $genere;
     public $anno;
     public $durata;
+
+    function __construct($_titolo, $_genere) {
+        $this->titolo = $_titolo;
+        $this->genere = $_genere;
+    }
 }
+
+$filmUno = new Movie('Il signore degli anelli', 'fantasy');
+//var_dump($filmUno);
+echo $filmUno->titolo .' ' .$filmUno->genere;
+
+$filmDue = new Movie('John Wick', 'azione');
+//var_dump($filmDue);
+echo $filmDue->titolo .' ' .$filmDue->genere;
