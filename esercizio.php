6d9a8fa4ea9d49c7c80a503d3c30183dd885863e
Fede <chiellofederico@gmail.com>
2021-03-12 19:05:43 +0100

    dichiaro il metodo

--- esercizio.php
+++ esercizio.php
@@ -1,21 +1,42 @@
 <?php
 
 class Movie {
+    // Variabili
     public $titolo;
     public $genere;
     public $anno;
     public $durata;
+    public $eta;
+    public $prezzo = 8;
+    public $sconto;
 
+    // Constructor
     function __construct($_titolo, $_genere) {
         $this->titolo = $_titolo;
         $this->genere = $_genere;
+    }
+
+    //Metodo
+    public function calcolaSconto($eta){
+        if($eta > 14) {
+            $this->sconto = 15;
+        } else {
+            $this->sconto = 20; 
+        }
+        return $this->prezzo - ($this->prezzo * $this->sconto / 100);
     }
 }
 
 $filmUno = new Movie('Il signore degli anelli', 'fantasy');
 //var_dump($filmUno);
-echo $filmUno->titolo .' ' .$filmUno->genere;
+echo $filmUno->titolo .' ' .$filmUno->genere .'<br>';
 
 $filmDue = new Movie('John Wick', 'azione');
 //var_dump($filmDue);
-echo $filmDue->titolo .' ' .$filmDue->genere;
+echo $filmDue->titolo .' ' .$filmDue->genere .'<br>';
+
+
+
+$prezzoBiglietto = $filmUno->calcolaSconto('15');
+$prezzoBiglietto2 = $filmUno->calcolaSconto('8');
+echo $prezzoBiglietto .'euro' .'<br>' .$prezzoBiglietto2 .'euro';
